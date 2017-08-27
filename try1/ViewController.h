18b78cf69c98c4ae90561739f71bc730922611da
smile <zhangshiyang@smile-2.local>
2017-08-27 15:13:36 -0500

    Initial Commit

---
+++ try1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  try1
+//
+//  Created by smile on 2017/8/27.
+//  Copyright © 2017年 smile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ try1Tests/try1Tests.m
@@ -0,0 +1,39 @@
+//
+//  try1Tests.m
+//  try1Tests
+//
+//  Created by smile on 2017/8/27.
+//  Copyright © 2017年 smile. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface try1Tests : XCTestCase
+
+@end
+
+@implementation try1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
